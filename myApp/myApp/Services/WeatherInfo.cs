94305607981e7cde60f04db5c506ce3b20213ef2
Darren Regan <darren111894@gmail.com>
2019-04-03 18:34:41 +0100

    Added GetWeather method which should hopefully get json data - Have to import NuGet Packages: newtonsoft json and Microsoft.CSharp package

--- myApp/myApp/Services/WeatherInfo.cs
+++ myApp/myApp/Services/WeatherInfo.cs
@@ -25,6 +25,17 @@ namespace myApp.Services
             if (results["weather"] != null)
             {
                 Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
                 return weather;
             }
             else
