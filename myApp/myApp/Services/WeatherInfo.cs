87000bbcf1f3c55534205c1e05582d57c7c65213
Darren Regan <darren111894@gmail.com>
2019-04-04 12:16:43 +0100

    Added a xaml template i found and just replaced Bindings to the correct names

--- myApp/myApp/Services/WeatherInfo.cs
+++ myApp/myApp/Services/WeatherInfo.cs
@@ -27,17 +27,17 @@ namespace myApp.Services
                 //JSON Data from http://api.openweathermap.org/data/2.5/weather?zip= + Zipcodes and key etc.
                 //Will display weather data from user entered zipcode
                 Weather weather = new Weather();
-                weather.Title = (string)results["name"];
-                weather.Temperature = (string)results["main"]["temp"] + " F";
-                weather.Wind = (string)results["wind"]["speed"] + " mph";
-                weather.Humidity = (string)results["main"]["humidity"] + " %";
-                weather.Visibility = (string)results["weather"][0]["main"];
+                //weather.Title = (string)results["name"];
+                //weather.Temperature = (string)results["main"]["temp"] + " F";
+                //weather.Wind = (string)results["wind"]["speed"] + " mph";
+                //weather.Humidity = (string)results["main"]["humidity"] + " %";
+                //weather.Visibility = (string)results["weather"][0]["main"];
 
-                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
-                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
-                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
-                weather.Sunrise = sunrise.ToString() + " UTC";
-                weather.Sunset = sunset.ToString() + " UTC";
+                //DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                //DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                //DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                //weather.Sunrise = sunrise.ToString() + " UTC";
+                //weather.Sunset = sunset.ToString() + " UTC";
                 return weather;
             }
             else
