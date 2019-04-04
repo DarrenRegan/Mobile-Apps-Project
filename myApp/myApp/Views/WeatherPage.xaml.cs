4db6c9f5a3cc21edfda9cc44d94a03e0fcf84416
Darren Regan <darren111894@gmail.com>
2019-04-04 12:19:36 +0100

    Added on Button click to request JSON data from API

--- myApp/myApp/Views/WeatherPage.xaml.cs
+++ myApp/myApp/Views/WeatherPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using myApp.Models;
+using myApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,25 @@ namespace myApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WeatherPage : ContentPage
 	{
+        RestService _restService;
+
 		public WeatherPage ()
 		{
 			InitializeComponent ();
+            _restService = new RestService();
 		}
-	}
-}
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        private string GenerateRequestUri(string openWeatherMapEndpoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
