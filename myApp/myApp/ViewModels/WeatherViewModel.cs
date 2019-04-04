e191a5a744830971a4d9146b4bf9896c7a8ac937
Darren Regan <darren111894@gmail.com>
2019-04-04 11:50:55 +0100

    Couldn't get weather working with my approach, trying another

---
+++ myApp/myApp/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace myApp.ViewModels
+{
+    public class WeatherViewModel : BaseViewModel
+    {
+        public WeatherViewModel()
+        {
+            Title = "Weather";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://www.rte.ie/weather/24237-galway/")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
