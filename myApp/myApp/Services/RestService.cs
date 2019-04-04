39c91924b1332400abb86c3fe4cb782679a7c753
Darren Regan <darren111894@gmail.com>
2019-04-04 15:35:29 +0100

    Made some changes to fonts and Img sizes - ADDED Test Plan aswell

--- myApp/myApp/Services/RestService.cs
+++ myApp/myApp/Services/RestService.cs
@@ -23,13 +23,18 @@ namespace myApp.Services
             WeatherData weatherData = null;
             try
             {
+                //Get Request
                 var response = await client.GetAsync(query);
+                //If receive back HTTP Status code between 200-299, read in the data received from the get Request
                 if(response.IsSuccessStatusCode)
                 {
+                    //Serialize the HTTP Content to a string called content
                     var content = await response.Content.ReadAsStringAsync();
+                    //JSON convert and deserialize content as WeatherData Object
                     weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
                 }
             }
+            //Prints out Error + exception if http request fails
             catch (Exception ex)
             {
                 Debug.WriteLine("\t\tERROR {0}", ex.Message);
--- myApp/myApp/ViewModels/AboutViewModel.cs
+++ myApp/myApp/ViewModels/AboutViewModel.cs
@@ -11,7 +11,7 @@ namespace myApp.ViewModels
         {
             Title = "About";
 
-            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://www.rte.ie/weather/24237-galway/")));
         }
 
         public ICommand OpenWebCommand { get; }
