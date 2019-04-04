0d7b3c97dd44182f67b84ed61a1282ec364290b7
Darren Regan <darren111894@gmail.com>
2019-04-04 11:56:55 +0100

    Added New files to try get weather to work including a LongToDateTimeConverter

---
+++ myApp/myApp/Services/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myApp.Services
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "7f3dafa83e83a2e1e812defb7e36141c";
+    }
+}
---
+++ myApp/myApp/Services/LongToDateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace myApp.Services
+{
+    public class LongToDateTimeConverter : IValueConverter
+    {
+        DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long dateTime = (long)value;
+            return $"{time.AddSeconds(dateTime).ToString()} UTC";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
