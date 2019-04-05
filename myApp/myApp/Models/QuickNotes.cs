2f063aea63f517ee564fb294b3239d1fc611d353
Darren Regan <darren111894@gmail.com>
2019-04-05 20:54:20 +0100

    Added some start SQLite code

---
+++ myApp/myApp/Models/QuickNotes.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myApp.Models
+{
+    public class QuickNotes
+    {
+        //Note and CreatedTImep represent columns in SQLite database, which are in table QuickNotes 
+        //ID is a unique ID with Primary key and an auto-increment
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+        public QuickNotes()
+        {
+            //default constructor
+        }
+    }
+}
---
+++ myApp/myApp/Services/QuickNotesDataBase.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+using myApp.Models;
+
+namespace myApp.Services
+{
+    public class QuickNotesDataBase
+    {
+        private SQLiteConnection _connection;
+        private DateTime CreatedTime;
+
+        public QuickNotesDataBase()
+        {
+            //Implements SQLiteInterface and calls GetConnection method
+            //CreateTable creates a table called QuickNotes
+            _connection = DependencyService.Get<SQLiteInterface>().GetConnection();
+            _connection.CreateTable<QuickNotes>();
+
+        }
+
+        public IEnumerable<QuickNotes> GetNotes()
+        {
+            return (from t in _connection.Table<QuickNotes>() select t).ToList();
+        }
+
+        public QuickNotes GetNote(int id)
+        {
+            return _connection.Table<QuickNotes>().FirstOrDefault(t => t.ID == id);
+        }
+
+        public void DeleteNote(int id)
+        {
+            _connection.Delete<QuickNotes>(id);
+        }
+
+        public void AddNote(string note) {
+            var newNote = new QuickNotes{
+                Note = note,
+                CreatedTime = DateTime.Now
+            };
+            _connection.Insert(newNote);
+        }//AddNote
+       
+    }
+}//myAPP
