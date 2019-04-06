975d87636ffae5311af7449167f086e6e8f82bfe
Darren Regan <darren111894@gmail.com>
2019-04-06 23:29:10 +0100

    Used transforms to make a black circle appear in centre of screen

--- README.md
+++ README.md
@@ -7,5 +7,14 @@
 * NetStanard.Libary
 * sqlite-net-pcl
 * SkiaSharp
-* SkiaSharp.Views
 * SKiaSharp.Views.Forms
+
+## SkiaGrapics
+ ### I picked SKiaGraphics because it looked like something we done in Graphics Programming last Semester and would be useful to know for the future
+ * Graphics System: SkiaGraphics Engine C++ project made by Google
+ * Immediate mode graphics system - When your program calls graphics drawing functions the graphics are quickly rendered
+ * Alternative: Retain mode graphics system where graphics are persistant and render whenever necessary
+ * Used for 2D graphics and used in Google chrome, Firefox and Android
+ * Draw Graphics on a SKCancasView or a SKGLView
+ * SKCanvas is used to call all drawing functions Translates, Scales, Rotations etc.
+ * SKPaint Draw Attributes: Style, Color, StrokeWidth, StrokeCap, StrokeJoin & Shader
--- myApp/myApp/Views/SkiaClock.xaml.cs
+++ myApp/myApp/Views/SkiaClock.xaml.cs
@@ -10,7 +10,8 @@ using System.Reflection;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 
-/* Graphics System: SkiaGraphics Engine C++ project made by Google
+/* I picked SKiaGraphics because it looked like something we done in Graphics Programming last Semester and would be useful to know for the future
+ * Graphics System: SkiaGraphics Engine C++ project made by Google
  * Immediate mode graphics system - When your program calls graphics drawing functions the graphics are quickly rendered
  * Alternative: Retain mode graphics system where graphics are persistant and render whenever necessary
  * Used for 2D graphics and used in Google chrome, Firefox and Android
@@ -23,22 +24,49 @@ namespace myApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkiaClock : ContentPage
 	{
-		public SkiaClock ()
+
+        //SKPaint Methods/Fields
+
+        SKPaint blackFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Black
+        };
+
+        //Main
+        public SkiaClock ()
 		{
 			InitializeComponent ();
 		}
 
-        //SKPaint Methods
 
-        //
+
+        
 
 
         private void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
+            //SKCanvas is used to call all drawing functions Translates, Scales, Rotations etc.
+            //SKPaint Draw Attributes: Style, Color, StrokeWidth, StrokeCap, StrokeJoin & Shader
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
 
+            //Variables for pixels from the event arguments
+            int width = e.Info.Width;
+            int height = e.Info.Height;
+
+            //Apply Transforms
+            canvas.Translate(width / 2, height / 2);
+            canvas.Scale(width / 200f);
+
+
+            //Clear to make a blank canvas
             canvas.Clear(SKColors.CornflowerBlue);
+
+
+
+            //Clock Background, centre at 0,0 radius = 100
+            canvas.DrawCircle(0, 0, 100, blackFillPaint);
 
         }
 	}
